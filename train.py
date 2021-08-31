438e1a7edabad10557472ce595c182350a30ffe5
mag389 <mag389@cornell.edu>
2021-08-31 16:23:05 +0000

    train file updated with newer loss function, and syntax fixes

--- train.py
+++ train.py
@@ -23,11 +23,13 @@ def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
     ds = Dataset(file_path, batch_size, max_len)
 
     # create the transformer architecture
-    transformer = Encoder(6, 1024, 8, 2048, 250)
+    transformer = Encoder(N, dm, h, hidden, max_len)
 
     # create loss object to use in custom loss function
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction='none')
+    # may use later for pen state
+    # loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    #     from_logits=True, reduction='none')
+    loss_object = tf.keras.losses.MeanSquaredError()
 
     # create masks function for training
     def create_mask(mask_rate, length):
@@ -43,13 +45,15 @@ def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
             pred: the predicted vlaues from the model
             mask: mask to block off parts of real and pred
         """
-        if mask:
+        if mask is not None:
+            mask = tf.logical_not(tf.math.equal(mask, 0))
+            mask = tf.cast(mask, dtype=real.dtype)
             masked_real = mask * real
             masked_pred = mask * pred
             loss_ = loss_object(masked_real, masked_pred)
             return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
         else:
-            loss = loss_object(real, pred)
+            loss_ = loss_object(real, pred)
             return tf.reduce_sum(loss_)
 
     # initialize some hyper param choices we'll need later
@@ -62,17 +66,17 @@ def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
     # train_step function
     @tf.function(input_signature=train_step_signature)
     def train_step(inp, tar):
-        """ single train step 
+        """ single train step
             takes in input and a target value
         """
-        # create mask(hide_rate)
-        train_mask = create_mask(rate, batch_size, length)
+        # create mask (rate, max_len) rate is not yet passed, will be later
+        train_mask = create_mask(.3, max_len)
 
         # crate the target input vs real this is where we'll do our masking
         tar_real = tar
         tar_masked = tar * train_mask
 
-        # then create masks, at least 2/3 now not needed because we went encoder only
+        # then create masks, at least 2/3 unneeded because we went encoder only
         # enc_p_mask, comb_mask, dec_p_mask = create_masks(inp, tar_inp)
 
         with tf.GradientTape() as tape:
@@ -82,9 +86,11 @@ def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
                                       mask=None)
             # i put in third arg but not yet totally sure
             loss = loss_function(tar_real, predictions, mask=train_mask)
+
         gradients = tape.gradient(loss, transformer.trainable_variables)
         optimizer.apply_gradients(
              zip(gradients, transformer.trainable_variables))
+
         train_loss(loss)
 
     # process through epochs training
@@ -99,3 +105,22 @@ def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
         print(f'Epoch {epoch + 1} Loss {train_loss.result():.4f}' +
               ' Accuracy {train_accuracy.result():.4f}')
     return transformer
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """ Custom learning rate schedule """
+    def __init__(self, d_model, warmup_steps=4000):
+        """ Init """
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        """ Call """
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
